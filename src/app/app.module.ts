1b0145518a8003d80fd6cdbc7b68c1a9b99585e7
Yash Gajera <yashgajera6202@gmail.com>
2022-05-18 13:52:20 +0530

    component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FeaturesComponent } from './features/features.component';
+import { PlansComponent } from './plans/plans.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BeforeAfterComponent } from './before-after/before-after.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    FeaturesComponent,
+    PlansComponent,
+    TestimonialComponent,
+    BeforeAfterComponent
   ],
   imports: [
     BrowserModule,
